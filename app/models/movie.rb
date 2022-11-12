55c527f209bc947f0bc2be19098dd05675095a05
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-11-11 23:00:14 -0800

    commit tree 886fc659b776b9ab09bebfaf6fd756da49d35a2f

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ApplicationRecord
+
+  def create
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  end
+  
+  private
+  
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
+
 end
